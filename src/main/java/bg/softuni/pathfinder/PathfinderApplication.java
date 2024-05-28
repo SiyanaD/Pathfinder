01a9e9aed5935106b500a81d3654aecffb0b9630
Siyana Danchilova <120177143+SiyanaD@users.noreply.github.com>
2024-05-28 05:20:55 +0300

    Update

--- src/main/java/bg/softuni/pathfinder/PathfinderApplication.java
+++ src/main/java/bg/softuni/pathfinder/PathfinderApplication.java
@@ -1,5 +1,6 @@
 package bg.softuni.pathfinder;
 
+import bg.softuni.pathfinder.model.UserRoles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class PathfinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PathfinderApplication.class, args);
 	}
+
 
 }

2d724bf7d1061f7fe2cf450757dcc5dea14e9cf3
Siyana Danchilova <120177143+SiyanaD@users.noreply.github.com>
2024-06-09 00:29:17 +0300

    update

--- src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -1,5 +1,18 @@
 package bg.softuni.pathfinder.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
-    
+    @GetMapping("users/register")
+    public String viewRegister(){
+        return "register";
+    }
+
+    @PostMapping("users/register")
+    public String doRegister(){
+        return "index";
+    }
 }

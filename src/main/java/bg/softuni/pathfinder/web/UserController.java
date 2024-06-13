cd2fa3f0879e6fcd5b8e62025312a963e71e5e20
Siyana Danchilova <120177143+SiyanaD@users.noreply.github.com>
2024-06-13 23:13:50 +0300

    Update

--- src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -3,6 +3,7 @@ package bg.softuni.pathfinder.web;
 import bg.softuni.pathfinder.web.dto.UserRegisterDTO;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class UserController {
     @GetMapping("users/register")
-    public String viewRegister(){
+    public String viewRegister(Model model){
+        model.addAttribute("registerData",new UserRegisterDTO());
         return "register";
     }
 
